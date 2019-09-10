7b9a55f5888f7ab31cb8eb5d96979183450df521
Arturo Corona <zkdnaem@gmail.com>
2019-09-10 18:19:10 -0500

    Child elements to Approval cards

--- componentsapp1/src/index.js
+++ componentsapp1/src/index.js
@@ -7,25 +7,39 @@ import faker from "faker";
 const App = () => {
   return (
     <div className="ui container comments">
-      <ApprovalCard />
-      <CommentDetail
-        author="Sam"
-        timeAgo="Today at 4:45PM"
-        img={faker.image.avatar()}
-        commentText="abcdefgojklmn"
-      />
-      <CommentDetail
-        author="Alex"
-        timeAgo="Today at 2:00AM"
-        img={faker.image.avatar()}
-        commentText="abcdefgojklmn"
-      />
-      <CommentDetail
-        author="Jane"
-        timeAgo="Yesterday at 5:00PM"
-        img={faker.image.avatar()}
-        commentText="abcdefgojklmn"
-      />
+      <ApprovalCard>
+        <div>
+          <h4>Warning!</h4>
+        </div>
+        Are you sure you want to do this?
+      </ApprovalCard>
+
+      <ApprovalCard>
+        <CommentDetail
+          author="Sam"
+          timeAgo="Today at 4:45PM"
+          img={faker.image.avatar()}
+          commentText="abcdefgojklmn"
+        />
+      </ApprovalCard>
+
+      <ApprovalCard>
+        <CommentDetail
+          author="Alex"
+          timeAgo="Today at 2:00AM"
+          img={faker.image.avatar()}
+          commentText="abcdefgojklmn"
+        />
+      </ApprovalCard>
+
+      <ApprovalCard>
+        <CommentDetail
+          author="Jane"
+          timeAgo="Yesterday at 5:00PM"
+          img={faker.image.avatar()}
+          commentText="abcdefgojklmn"
+        />
+      </ApprovalCard>
     </div>
   );
 };
