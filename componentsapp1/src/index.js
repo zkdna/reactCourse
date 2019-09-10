2d275ae091f0125abfe117829d5ac9decc92d033
Arturo Corona <zkdnaem@gmail.com>
2019-09-10 08:57:45 -0500

    Content showed using the props system

--- componentsapp1/src/index.js
+++ componentsapp1/src/index.js
@@ -1,11 +1,29 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import CommentDetail from "./CommentDetail";
+import faker from "faker";
 
 const App = () => {
   return (
     <div className="ui container comments">
-      <CommentDetail />
+      <CommentDetail
+        author="Sam"
+        timeAgo="Today at 4:45PM"
+        img={faker.image.avatar()}
+        commentText="abcdefgojklmn"
+      />
+      <CommentDetail
+        author="Alex"
+        timeAgo="Today at 2:00AM"
+        img={faker.image.avatar()}
+        commentText="abcdefgojklmn"
+      />
+      <CommentDetail
+        author="Jane"
+        timeAgo="Yesterday at 5:00PM"
+        img={faker.image.avatar()}
+        commentText="abcdefgojklmn"
+      />
     </div>
   );
 };
