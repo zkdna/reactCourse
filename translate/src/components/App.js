410d94cde8d7e6ca24d4051bdf096a9bcd420d7d
zkdna <zkdnaem@gmail.com>
2020-03-31 13:49:17 -0600

    Context example

--- translate/src/components/App.js
+++ translate/src/components/App.js
@@ -1,8 +1,36 @@
 import React from "react";
+import UserCreate from "./UserCreate";
+import LanguageContext from "../contexts/LanguageContext";
+import ColorContext from "../contexts/ColorContext";
 
 class App extends React.Component {
+  state = { language: "english" };
+
+  onLanguageChange = language => {
+    this.setState({ language });
+  };
+
   render() {
-    return <div>App</div>;
+    return (
+      <div className="ui container">
+        <div>
+          Select a language:
+          <i
+            className="flag us"
+            onClick={() => this.onLanguageChange("english")}
+          />
+          <i
+            className="flag mx"
+            onClick={() => this.onLanguageChange("spanish")}
+          />
+        </div>
+        <ColorContext.Provider value="red">
+          <LanguageContext.Provider value={this.state.language}>
+            <UserCreate />
+          </LanguageContext.Provider>
+        </ColorContext.Provider>
+      </div>
+    );
   }
 }
 
