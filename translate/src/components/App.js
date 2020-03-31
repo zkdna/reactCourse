d4405b168fe91dab7fe8dbb57c521a032f13a919
zkdna <zkdnaem@gmail.com>
2020-03-31 09:31:59 -0600

    Redo translate

--- translate/src/components/App.js
+++ translate/src/components/App.js
@@ -1,30 +1,8 @@
 import React from "react";
-import UserCreate from "./UserCreate";
 
 class App extends React.Component {
-  state = { language: "english" };
-
-  onLanguageChange = language => {
-    this.setState({ language });
-  };
-
   render() {
-    return (
-      <div className="ui container">
-        <div>
-          Select a language:
-          <i
-            className="flag us"
-            onClick={() => this.onLanguageChange("english")}
-          />
-          <i
-            className="flag mx"
-            onClick={() => this.onLanguageChange("spanish")}
-          />
-        </div>
-        <UserCreate />
-      </div>
-    );
+    return <div>App</div>;
   }
 }
 
